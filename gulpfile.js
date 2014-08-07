2247a76ad70e909d02f719f6f774eeaae7ff85b0
Andrew Sullivan <licyeus@gmail.com>
2014-08-06 22:20:21 -0700

    New gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
-var gulp       = require('gulp'),
-    livereload = require('gulp-livereload')
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint'),
     connect = require('gulp-connect');
 
-gulp.task('server', function() {
-  connect.server();
-});
-
-gulp.task('watch', ['server'], function() {
-  var server = livereload();
-  gulp.watch(['js/**', 'css/**', 'partials/**', 'index.html']).on('change', function(file) {
-    server.changed(file.path);
+gulp.task('connectDev', function () {
+  connect.server({
+    root: '.',
+    port: 8000,
+    livereload: true
   });
 });
 
-gulp.task('dev', ['watch']);
+gulp.task('less', function() {
+  return gulp.src('css/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('css/'))
+    .pipe(connect.reload());
+});
 
+gulp.task('html', function () {
+  gulp.src('**/*.html').pipe(connect.reload());
+});
+
+gulp.task('scripts', function () {
+  gulp.src('src/**/*.js').pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./**/*.html', ['html']);
+  gulp.watch('css/**/*.less', ['less']);
+  gulp.watch('src/**/*.js', ['scripts']);
+});
+
+gulp.task('dev', ['connectDev', 'watch']);
